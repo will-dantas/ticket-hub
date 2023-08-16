fba5e97a8fd24a0c52493bc1a3af5077c66910ab
will-dantas <wmdantas2@gmail.com>
2023-08-15 23:08:08 -0300

    feat: Add middlewares Error

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application } from 'express';
 import { connect } from './infra/database';
+import { errorMiddleware } from './middlewares/error.middlewares';
 
 class App {
   public app: Application;
@@ -16,7 +17,7 @@ class App {
   }
 
   interceptionError() {
-    // this.app.use( )
+    this.app.use(errorMiddleware);
   }
 
   middlewaresInitalize() {
---
+++ src/middlewares/error.middlewares.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Request, Response } from 'express';
+import { Httpexception } from '../interfaces/Httpexceptions';
+
+export function errorMiddleware (err: Httpexception, req: Request, res: Response, next: NextFunction) {
+  const status: number = err.status ?? 500;
+  const message: string = err.message ?? 'Internal Server Error';
+
+  res.status(status).json({
+    status,
+    message
+  });
+}
